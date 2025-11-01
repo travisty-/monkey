5b4b96f9a047d1012b5b9ca15259987d78ec3c78
Travis Kinney <travis@traviskinney.co>
2025-10-31 22:41:19 -0700

    Add initial lexer implementation

---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		_, _ = fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			_, _ = fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
